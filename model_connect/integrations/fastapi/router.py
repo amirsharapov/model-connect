7e13c9d74f81d263a7207b4c1d43ec59e2a43dbe
Amir Sharapov <amir@amirfounder.com>
2023-08-03 18:58:27 -0500

    model connect changes. attach route rfunction

--- model_connect/integrations/fastapi/router.py
+++ model_connect/integrations/fastapi/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, FastAPI
 
 from model_connect.registry import get_model
 
@@ -14,3 +14,18 @@ def create_router(
         prefix=options.resource_path if add_prefix else '',
         tags=[options.tag_name] if add_tags else []
     )
+
+
+def attach_router(
+        app: FastAPI,
+        dataclass_type: type,
+        add_prefix: bool = True,
+        add_tags: bool = True
+):
+    router = create_router(
+        dataclass_type,
+        add_prefix=add_prefix,
+        add_tags=add_tags
+    )
+
+    app.include_router(router)
