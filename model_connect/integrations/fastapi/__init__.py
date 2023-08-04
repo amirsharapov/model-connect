bb2f0a700bd1e199c435c4f00ff3dd67e29f3238
Amir Sharapov <amir@amirfounder.com>
2023-08-03 19:22:17 -0500

    import error fix

--- model_connect/integrations/fastapi/__init__.py
+++ model_connect/integrations/fastapi/__init__.py
@@ -1,6 +1,5 @@
 from model_connect.integrations.fastapi.options.model import FastAPIModel
 from model_connect.integrations.fastapi.options.model_field import FastAPIModelField
-from model_connect.integrations.fastapi.router import create_router
 
 from model_connect.integrations import registry as _integrations_registry
 
