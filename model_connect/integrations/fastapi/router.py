b1a8f8124b7249c7e7ddff2a4e2a3a582ff8301f
Amir Sharapov <amir@amirfounder.com>
2023-08-03 19:02:02 -0500

    attach router better

--- model_connect/integrations/fastapi/router.py
+++ model_connect/integrations/fastapi/router.py
@@ -3,29 +3,18 @@ from fastapi import APIRouter, FastAPI
 from model_connect.registry import get_model
 
 
-def create_router(
-        dataclass_type: type,
-        add_prefix: bool = True,
-        add_tags: bool = True
-):
-    options = get_model(dataclass_type, 'fastapi')
-
-    return APIRouter(
-        prefix=options.resource_path if add_prefix else '',
-        tags=[options.tag_name] if add_tags else []
-    )
-
-
 def attach_router(
         app: FastAPI,
         dataclass_type: type,
-        add_prefix: bool = True,
-        add_tags: bool = True
+        router: APIRouter
 ):
-    router = create_router(
+    model = get_model(
         dataclass_type,
-        add_prefix=add_prefix,
-        add_tags=add_tags
+        'fastapi'
     )
 
-    app.include_router(router)
+    app.include_router(
+        router,
+        prefix=model.resource_path,
+        tags=[model.tag_name]
+    )
